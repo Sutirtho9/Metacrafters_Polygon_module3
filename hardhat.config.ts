3f28383257d74e20bf20fb8fd69aecfd18d17224
Sutirtho9 <93462188+Sutirtho9@users.noreply.github.com>
2023-08-06 21:25:41 +0530

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,7 +4,7 @@ import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-circom";
 // circuits
 import circuits = require('./circuits.config.json')
-
+import 'dotenv/config';
 // set env var to the root of the project
 process.env.BASE_PATH = __dirname;
 
@@ -21,6 +21,12 @@ const config: HardhatUserConfig = {
         version: "0.6.11",
       }
     ]
+  },
+  networks: {
+    mumbai: {
+      url: `https://rpc.ankr.com/polygon_mumbai`,
+      accounts: [process.env.MUMBAI_PRIVATE_KEY],
+    },
   },
   circom: {
     // (optional) Base path for input files, defaults to `./circuits/`
